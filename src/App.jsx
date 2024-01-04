c5ea366b2b3ba5afed9266863bb8517c1f0252b9
hisbanshiraz <hisbanbardai@gmail.com>
2024-01-04 08:22:21 -0700

    Pass data as props to the Result component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,72 @@
-import Calculator from "./components/Calculator";
+import { useState } from "react";
 import Result from "./components/Result";
 import UserInput from "./components/UserInput";
+import { calculateInvestmentResults } from "./util/investment";
 
 function App() {
+  const [initialInvestment, setInitialInvestment] = useState(0);
+  const [annualInvestment, setAnnualInvestment] = useState(0);
+  const [expectedReturn, setExpectedReturn] = useState(0);
+  const [duration, setDuration] = useState(0);
+
+  function handleInitialInvestmentInputChange(event) {
+    setInitialInvestment(Number(event.target.value));
+  }
+
+  function handleAnnualInvestmentInputChange(event) {
+    setAnnualInvestment(Number(event.target.value));
+  }
+
+  function handleExpectedReturnInputChange(event) {
+    setExpectedReturn(Number(event.target.value));
+  }
+
+  function handleDurationInputChange(event) {
+    setDuration(Number(event.target.value));
+  }
+
+  console.log(initialInvestment, annualInvestment, expectedReturn, duration);
+
+  // const results = calculateInvestmentResults({
+  //   initialInvestment,
+  //   annualInvestment,
+  //   expectedReturn,
+  //   duration,
+  // });
+
+  // console.log(duration);
+
   return (
     <>
       <div id="user-input">
-        <UserInput>INITIAL INVESTMENT</UserInput>
-        <UserInput>ANNUAL INVESTMENT</UserInput>
-        <UserInput>EXPECTED RETURN</UserInput>
-        <UserInput>DURATION</UserInput>
+        <UserInput
+          value={initialInvestment}
+          onChange={handleInitialInvestmentInputChange}
+        >
+          INITIAL INVESTMENT
+        </UserInput>
+        <UserInput
+          value={annualInvestment}
+          onChange={handleAnnualInvestmentInputChange}
+        >
+          ANNUAL INVESTMENT
+        </UserInput>
+        <UserInput
+          value={expectedReturn}
+          onChange={handleExpectedReturnInputChange}
+        >
+          EXPECTED RETURN
+        </UserInput>
+        <UserInput value={duration} onChange={handleDurationInputChange}>
+          DURATION
+        </UserInput>
       </div>
-      <Result />
+      <Result
+        initialInvestment={initialInvestment}
+        annualInvestment={annualInvestment}
+        expectedReturn={expectedReturn}
+        duration={duration}
+      />
     </>
   );
 }
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,9 +1,9 @@
-export default function UserInput({ children }) {
+export default function UserInput({ children, value, onChange }) {
   return (
     <div className="input-group">
       <label htmlFor="">
         {children}
-        <input type="number" />
+        <input type="number" onChange={onChange} value={value} />
       </label>
     </div>
   );
